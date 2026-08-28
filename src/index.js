4625f29b4bb633d95104b11ce8802218841cf635
khfy7wpr5p-maker <onderozudogru@gmail.com>
2026-08-28 10:44:37 +0300

    E10D: export approved corpus and mutation cases

--- src/index.js
+++ src/index.js
@@ -23,3 +23,5 @@ export { CORPUS_SOURCE_STATUS, createCorpusSource, promoteCorpusSourceForGold } 
 export { REFERENCE_CORPUS, getReferenceCorpusSource } from './benchmark/referenceCorpusRegistry.js'
 export { REVIEW_PACKET_STATUS, createTeacherReviewPacket, approveTeacherReviewPacket, rejectTeacherReviewPacket, promoteReviewedSourceForGold } from './benchmark/teacherReviewPacket.js'
 export { REFERENCE_REVIEW_QUEUE, getReferenceReviewPacket } from './benchmark/referenceReviewQueue.js'
+export { TEACHER_APPROVALS, APPROVED_REVIEW_PACKETS, GOLD_ELIGIBLE_REFERENCE_CORPUS, getGoldEligibleReferenceSource } from './benchmark/approvedReferenceRegistry.js'
+export { TEACHER_APPROVED_MUTATION_CASES } from './benchmark/teacherApprovedMutationCases.js'
