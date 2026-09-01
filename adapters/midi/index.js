c7209130fc990454c115fae80f8c4383c73039da
khfy7wpr5p-maker <onderozudogru@gmail.com>
2026-09-01 21:00:47 +0300

    CE-MIDI-04: export conservative repeated-pitch classifier

--- adapters/midi/index.js
+++ adapters/midi/index.js
@@ -1,5 +1,6 @@
 export { loadMidiReference, parseMidiReference } from './midiReferenceAdapter.js'
 export { DEFAULT_MIDI_ALIGNMENT_OPTIONS, extractScoreReferenceEvents, estimateMidiScoreAlignment, analyzeMidiScoreAlignment } from './midiScoreAlignment.js'
+export { reclassifyPolyphonicRepeatedPitchAmbiguity, analyzeMidiScoreAlignmentConservatively } from './polyphonicRepeatedPitchAmbiguity.js'
 export {
   MIDI_INSTRUMENT_CONTRACT_VERSION,
   createMidiInstrumentContract,
